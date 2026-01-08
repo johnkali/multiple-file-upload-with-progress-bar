3a116bb5288b06866c61c9a48c0838027b0ed2bd
John <jdkali96@gmail.com>
2026-01-08 23:00:42 +0300

    Put FileInput into its own component

---
+++ src/components/FileInput.tsx
@@ -0,0 +1,32 @@
+import React, { type ChangeEvent } from "react";
+import { Plus } from "lucide-react";
+type FileInputPops = {
+  inputRef: React.RefObject<HTMLInputElement | null>;
+  disabled: boolean;
+  onFileSelect: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+function FileInput({ inputRef, disabled, onFileSelect }: FileInputPops) {
+  return (
+    <>
+      <input
+        type="file"
+        ref={inputRef}
+        onChange={onFileSelect}
+        multiple
+        className="hidden"
+        id="file-upload"
+        disabled={disabled}
+      />
+      <label
+        htmlFor="file-upload"
+        className="flex cursor-pointer items center gap-2 rounded-md bg-grayscale-700 px-6 py-2 hover:opacity-90"
+      >
+        <Plus size={18} />
+        Select Files
+      </label>
+    </>
+  );
+}
+
+export default FileInput;
